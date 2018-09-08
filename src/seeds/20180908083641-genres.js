b4ad3888ff25bd9e83dbbb146563d2262a5a2af0
Javier López <javierlopez@uc.cl>
2018-09-08 05:49:13 -0300

    feat(books):set genre name to be unique

---
+++ src/seeds/20180908083641-genres.js
@@ -0,0 +1,17 @@
+const genresJson = require('./genres.json');
+
+module.exports = {
+  up: (queryInterface) => {
+    const genresData = [];
+    genresJson.genres.forEach(({ name }) => {
+      genresData.push({
+        name,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    });
+    return queryInterface.bulkInsert('Genres', genresData);
+  },
+
+  down: queryInterface => queryInterface.bulkDelete('Genres', null, {}),
+};
