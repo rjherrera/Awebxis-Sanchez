05c448ef241cda97ce8791328f2d6537b7072ee1
Javier López <javierlopez@uc.cl>
2018-09-08 00:16:08 -0300

    feat(books):add loadBook middleware

--- src/routes/books.js
+++ src/routes/books.js
@@ -2,13 +2,18 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter();
 
+const loadBook = async (ctx, next) => {
+  ctx.state.book = await ctx.orm.Book.findOne({ where: { isbn: ctx.params.isbn } });
+  return next();
+};
+
 router.get('books', '/', async (ctx) => {
   const page = ctx.query.page || 1;
   ctx.body = await ctx.orm.Book.findAll({ offset: (page - 1) * 10, limit: 10 });
 });
 
-router.get('books-show', '/:isbn', async (ctx) => {
-  const book = await ctx.orm.Book.findOne({ where: { isbn: ctx.params.isbn } });
+router.get('books-show', '/:isbn', loadBook, async (ctx) => {
+  const { book } = ctx.state;
   ctx.assert(book, 404);
   ctx.body = book;
 });
@@ -19,7 +24,7 @@ router.post('books-create', '/', async (ctx) => {
 });
 
 router.patch('books-update', '/:isbn', async (ctx) => {
-  const book = await ctx.orm.Book.findOne({ where: { isbn: ctx.params.isbn } });
+  const { book } = ctx.state;
   ctx.assert(book, 404);
   ctx.body = await book.update(
     ctx.request.body,
@@ -27,7 +32,7 @@ router.patch('books-update', '/:isbn', async (ctx) => {
 });
 
 router.delete('books-destroy', '/:isbn', async (ctx) => {
-  const book = await ctx.orm.Book.findOne({ where: { isbn: ctx.params.isbn } });
+  const { book } = ctx.state;
   ctx.assert(book, 404);
   await book.destroy();
   ctx.redirect(ctx.router.url('books'));
