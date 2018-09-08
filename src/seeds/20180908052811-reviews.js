c15f6c2e4fd768ce51070ea98ae601fb90117fb6
Javier López <javierlopez@uc.cl>
2018-09-08 05:15:42 -0300

    fix(books):fixed timeout reviews seed

--- src/seeds/20180908052811-reviews.js
+++ src/seeds/20180908052811-reviews.js
@@ -1,11 +1,13 @@
-const booksJson = require('./books.json');
+const Promise = require('bluebird');
+
 const { Book } = require('../models');
+const booksJson = require('./books.json');
 
 const getRandomRating = () => Math.floor(Math.random() * 5) + 1;
 
 module.exports = {
   async up(queryInterface) {
-    const reviewsBulkInsertPromises = await booksJson.books.map(async ({ isbn, reviews }) => {
+    const reviewsBulkInsertPromises = Promise.map(booksJson.books, async ({ isbn, reviews }) => {
       const reviewsData = [];
       const BookId = (await Book.findOne({ where: { isbn } })).id;
       reviews.forEach((review) => {
@@ -18,8 +20,9 @@ module.exports = {
         });
       });
       return queryInterface.bulkInsert('Reviews', reviewsData);
-    });
-    return Promise.all(reviewsBulkInsertPromises);
+    }, { concurrency: 1000 });
+
+    return reviewsBulkInsertPromises;
   },
 
   down: queryInterface => queryInterface.bulkDelete('Reviews', null, {}),
