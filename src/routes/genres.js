960ada4ae2b8e44f56e9dd21f2aa1722925ab16b
Javier López <javierlopez@uc.cl>
2018-09-19 20:48:53 -0300

    feat(books):add id param middleware to genres router

--- src/routes/genres.js
+++ src/routes/genres.js
@@ -2,11 +2,12 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter();
 
-const loadGenre = async (ctx, next) => {
-  ctx.state.genre = await ctx.orm.Genre.findOne({ where: { id: ctx.params.id } });
-  ctx.assert(ctx.state.genre, 404);
+router.param('id', async (id, ctx, next) => {
+  const genre = await ctx.orm.Genre.findOne({ where: { id: ctx.params.id } });
+  ctx.assert(genre, 404);
+  ctx.state.genre = genre;
   return next();
-};
+});
 
 router.get('genres', '/', async (ctx) => {
   const genres = await ctx.orm.Genre.findAll();
@@ -23,7 +24,7 @@ router.get('genres-new', '/new', async (ctx) => {
   });
 });
 
-router.get('genres-edit', '/:id/edit', loadGenre, async (ctx) => {
+router.get('genres-edit', '/:id/edit', async (ctx) => {
   const { genre } = ctx.state;
   await ctx.render('genres/edit', {
     genre,
@@ -44,7 +45,7 @@ router.post('genres-create', '/', async (ctx) => {
   }
 });
 
-router.patch('genres-update', '/:id', loadGenre, async (ctx) => {
+router.patch('genres-update', '/:id', async (ctx) => {
   const { genre } = ctx.state;
   try {
     await genre.update(ctx.request.body);
@@ -58,7 +59,7 @@ router.patch('genres-update', '/:id', loadGenre, async (ctx) => {
   }
 });
 
-router.get('genres-show', '/:id', loadGenre, async (ctx) => {
+router.get('genres-show', '/:id', async (ctx) => {
   const { genre } = ctx.state;
   await ctx.render('genres/show', {
     genre,
@@ -66,7 +67,7 @@ router.get('genres-show', '/:id', loadGenre, async (ctx) => {
   });
 });
 
-router.delete('genres-destroy', '/:id', loadGenre, async (ctx) => {
+router.delete('genres-destroy', '/:id', async (ctx) => {
   const { genre } = ctx.state;
   await genre.destroy();
   ctx.redirect(ctx.router.url('genres'));
