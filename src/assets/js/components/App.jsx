68d8c51ca0299e78977fb8aa017aa1722cd0e34d
Raimundo Herrera <rjherrera@uc.cl>
2018-09-22 20:18:35 -0300

    feat(index): change title, content and text

--- src/assets/js/components/App.jsx
+++ src/assets/js/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 
 function App() {
-  return <div>Hello React World!</div>;
+  return <div>React Magic!</div>;
 }
 
 export default hot(module)(App);
