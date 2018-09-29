e470fe0656b0d6f922f4ccc1b44ae22eac2177d8
Javier López <javierlopez@uc.cl>
2018-09-29 17:08:11 -0300

    refactor(interests): remove unnecessary foreign key definitions from Interest model

--- src/models/interest.js
+++ src/models/interest.js
@@ -1,17 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
   const Interest = sequelize.define('Interest', {
-    userId: {
-      type: DataTypes.INTEGER,
-      references: 'User',
-      referencesKey: 'id',
-      allowNull: false,
-    },
-    bookId: {
-      type: DataTypes.INTEGER,
-      references: 'Book',
-      referencesKey: 'id',
-      allowNull: false,
-    },
   }, {});
   Interest.associate = (models) => {
     models.Book.belongsToMany(models.User, { through: Interest, as: 'interested', foreignKey: 'bookId' });
