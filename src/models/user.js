654895f5cd6a32ddbca2ee14a19444afbffc2ec3
Raimundo Herrera <rjherrera@uc.cl>
2018-10-10 01:13:00 -0300

    feat(user): password hashed on save and checkPassword function

--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: {
@@ -50,7 +52,19 @@ module.exports = (sequelize, DataTypes) => {
         },
       },
     },
-  }, {});
+  }, {
+    hooks: {
+      async beforeSave(instance) {
+        if (instance.changed('password')) {
+          instance.set('password', await bcrypt.hash(instance.password, 10));
+        }
+      },
+    },
+  });
+
+  User.prototype.checkPassword = function checkPassword(password) {
+    return bcrypt.compare(password, this.password);
+  };
 
   User.associate = (models) => {
     models.User.hasMany(models.Feedback, { as: 'feedbacks', foreignKey: 'feedbackeeId' });
