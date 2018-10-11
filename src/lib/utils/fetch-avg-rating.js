922baa77240bb33898ca1751da749614e660f606
jmcomber <jmcomber1@gmail.com>
2018-10-11 15:54:48 -0300

    fix(rating): use env variable for API key instead of unsafe value

--- src/lib/utils/fetch-avg-rating.js
+++ src/lib/utils/fetch-avg-rating.js
@@ -2,7 +2,8 @@ const fetch = require('node-fetch');
 
 const fetchAvgRating = async (book) => {
   const { isbn: bookISBN } = book;
-  const url = `https://www.goodreads.com/book/review_counts.json?key={M15y1EyDKjbpaeuJ5ahQ1w}&isbns=${bookISBN}`;
+  const key = process.env.GOODREADS_KEY;
+  const url = `https://www.goodreads.com/book/review_counts.json?key=${key}&isbns=${bookISBN}`;
   const response = await fetch(url).then(data => data.json().then(json => json));
   return response.books[0].average_rating;
 };
