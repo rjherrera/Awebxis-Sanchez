1fbd30b9a8b761d01393157820350d7919005e03
Raimundo Herrera <rjherrera@uc.cl>
2018-10-11 09:30:55 -0300

    feat(matches): remove try when there is no error handling

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -11,29 +11,21 @@ router.param('id', async (id, ctx, next) => {
 
 router.post('match-create', '/new', async (ctx) => {
   const match = await ctx.orm.Match.build(ctx.request.body);
-  try {
-    await match.save({ fields: ['proposerBookInstanceId', 'proposeeBookInstanceId'] });
-    ctx.redirect(ctx.router.url('books'));
-  } catch (e) {
-    ctx.redirect('users');
-  }
+  await match.save({ fields: ['proposerBookInstanceId', 'proposeeBookInstanceId'] });
+  ctx.redirect('back');
 });
 
 
 router.patch('match-accept', '/:id', async (ctx) => {
   const { match } = ctx.state;
-  try {
-    await match.accept();
-    ctx.redirect(ctx.router.url('books'));
-  } catch (error) {
-    ctx.redirect(ctx.router.url('users'));
-  }
+  await match.accept();
+  ctx.redirect('back');
 });
 
 router.delete('match-destroy', '/:id', async (ctx) => {
   const { match } = ctx.state;
   await match.destroy();
-  ctx.redirect(ctx.router.url('books'));
+  ctx.redirect('back');
 });
 
 module.exports = router;
