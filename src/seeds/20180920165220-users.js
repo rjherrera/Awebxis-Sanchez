f34880c638d782a793f5d586404bcc631a906572
Raimundo Herrera <rjherrera@uc.cl>
2018-10-13 15:37:03 -0300

    feat(seeds): add admins to user seed

--- src/seeds/20180920165220-users.js
+++ src/seeds/20180920165220-users.js
@@ -4,8 +4,9 @@ const getJSON = require('../lib/seeds/get-json');
 module.exports = {
   up: async (queryInterface) => {
     const usersJson = await getJSON('users.json');
-    const usersData = await Promise.all(
-      usersJson.map(async user => ({
+    const adminsJson = await getJSON('admins.json');
+    const usersData = await Promise.all([
+      ...usersJson.map(async user => ({
         username: user.username,
         firstName: user.first_name,
         lastName: user.last_name,
@@ -16,7 +17,18 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       })),
-    );
+      ...adminsJson.map(async user => ({
+        username: user.username,
+        firstName: user.first_name,
+        lastName: user.last_name,
+        email: user.email,
+        password: await bcrypt.hash(user.password, 10),
+        admin: true,
+        profilePicUrl: null,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      })),
+    ]);
     return queryInterface.bulkInsert('Users', usersData);
   },
 
