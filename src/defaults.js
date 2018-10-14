5001dd505c75660b9eb79ad4f6697f7784127e32
Raimundo Herrera <rjherrera@uc.cl>
2018-10-14 20:59:52 -0300

    feat(users): add default image for missing profile pic

--- src/defaults.js
+++ src/defaults.js
@@ -1,3 +1,4 @@
 module.exports = {
   bookImageUrl: 'https://storage.googleapis.com/webdesignledger.pub.network/LaT/edd/2016/02/seamless-book-cover-textures-1.jpg',
+  userImageUrl: 'http://hauertlab.com/wp-content/uploads/2016/05/headshot_placeholder.png',
 };
