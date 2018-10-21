988b008bd25b87ef44111cd377f8a771c5452105
Raimundo Herrera <rjherrera@uc.cl>
2018-10-21 12:20:19 -0300

    fix(mail): rename env vars to match heroku's sendgrid add-on

--- src/config/email.js
+++ src/config/email.js
@@ -4,8 +4,8 @@ module.exports = {
     service: 'SendGrid',
     // auth data always from ENV vars
     auth: {
-      user: process.env.SENDGRID_USER,
-      pass: process.env.SENDGRID_PASS,
+      user: process.env.SENDGRID_USERNAME,
+      pass: process.env.SENDGRID_PASSWORD,
     },
   },
   // defaults to be passed to nodemailer's emails
