4008252153368e456c7db32ce6c87fa94bc0c4ba
Raimundo Herrera <rjherrera@uc.cl>
2018-10-21 11:29:28 -0300

    feat(mail): change from/sender email

--- src/config/email.js
+++ src/config/email.js
@@ -10,6 +10,6 @@ module.exports = {
   },
   // defaults to be passed to nodemailer's emails
   defaults: {
-    from: 'template <template@example.org>',
+    from: 'Cambalache <contact@cambalache.com>',
   },
 };
