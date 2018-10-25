7538985279ee4db4db1b34046adcdec22687fcb6
Javier López <javierlopez@uc.cl>
2018-10-25 18:44:20 -0300

    feat(seed): remove randomization from book-instances

--- src/seeds/20181006025434-book-instances.js
+++ src/seeds/20181006025434-book-instances.js
@@ -1,18 +1,13 @@
 const getJSON = require('../lib/seeds/get-json');
-const { User, Book } = require('../models');
-
-const randint = n => Math.floor(Math.random() * n) + 1;
 
 module.exports = {
   up: async (queryInterface) => {
-    const users = await User.findAll({ attributes: ['id'] });
-    const books = await Book.findAll({ attributes: ['id'] });
     const instancesJson = await getJSON('book-instances.json');
     const instancesData = [];
     instancesJson.bookInstances.forEach((instance) => {
       instancesData.push({
-        userId: users[randint(users.length - 1)].id,
-        bookId: books[randint(books.length - 1)].id,
+        userId: instance.userId,
+        bookId: instance.bookId,
         state: instance.state,
         comment: instance.comment,
         createdAt: new Date(),
