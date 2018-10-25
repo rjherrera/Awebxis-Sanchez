3afbb5349789d1594f9639382fa20d87ca54aa25
Javier López <javierlopez@uc.cl>
2018-10-25 19:26:14 -0300

    feat(seed): fix book-instances seed ids

--- src/seeds/20181006025434-book-instances.js
+++ src/seeds/20181006025434-book-instances.js
@@ -1,15 +1,18 @@
 const getJSON = require('../lib/seeds/get-json');
+const { User, Book } = require('../models');
 
 module.exports = {
   up: async (queryInterface) => {
     const instancesJson = await getJSON('book-instances.json');
+    const users = await User.findAll({ attributes: ['id', 'username'] });
+    const books = await Book.findAll({ attributes: ['id', 'isbn'] });
     const instancesData = [];
     instancesJson.bookInstances.forEach((instance) => {
       instancesData.push({
-        userId: instance.userId,
-        bookId: instance.bookId,
-        state: instance.state,
-        comment: instance.comment,
+        userId: users.find(user => user.username === instance.username).id,
+        bookId: books.find(book => book.isbn === instance.isbn).id,
+        state: 4,
+        comment: 'Almost new, I promise.',
         createdAt: new Date(),
         updatedAt: new Date(),
       });
