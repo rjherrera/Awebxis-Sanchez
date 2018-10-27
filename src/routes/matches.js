d5e7cb3cdf2f857d4d510c3f7c093d9c25bbd9ab
jmcomber <jmcomber1@gmail.com>
2018-10-27 10:35:35 -0300

    style(new-proposal): shorten two lines

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -1,4 +1,6 @@
 const KoaRouter = require('koa-router');
+const sendNewProposalEmail = require('../mailers/new-proposal.js');
+
 
 const router = new KoaRouter();
 
@@ -11,6 +13,26 @@ router.param('id', async (id, ctx, next) => {
 
 router.post('match-create', '/new', async (ctx) => {
   const match = await ctx.orm.Match.build(ctx.request.body);
+
+  const bookInstanceProposee = await ctx.orm.BookInstance.findById(match.proposeeBookInstanceId);
+  const bookProposee = await ctx.orm.Book.findById(bookInstanceProposee.bookId);
+  const recipient = await ctx.orm.User.findById(bookInstanceProposee.userId);
+
+  const bookInstanceProposer = await ctx.orm.BookInstance.findById(match.proposerBookInstanceId);
+  const bookProposer = await ctx.orm.Book.findById(bookInstanceProposer.bookId);
+  const sender = await ctx.orm.User.findById(bookInstanceProposer.userId);
+
+  try {
+    sendNewProposalEmail(ctx, {
+      recipient,
+      bookProposee,
+      sender,
+      bookProposer,
+      origin: ctx.request.origin,
+    });
+  } catch (error) {
+    // Ignore if mail not sent, though it shouldn't fail
+  }
   await match.save({ fields: ['proposerBookInstanceId', 'proposeeBookInstanceId'] });
   ctx.redirect('back');
 });
@@ -18,6 +40,16 @@ router.post('match-create', '/new', async (ctx) => {
 
 router.patch('match-accept', '/:id', async (ctx) => {
   const { match } = ctx.state;
+
+  const bookInstanceProposee = await ctx.orm.BookInstance.findById(match.proposeeBookInstanceId);
+  const bookProposee = await ctx.orm.Book.findById(bookInstanceProposee.bookId);
+  const recipient = await ctx.orm.User.findById(bookInstanceProposee.userId);
+
+  const bookInstanceProposer = await ctx.orm.BookInstance.findById(ctx.request.body.proposerBookInstanceId);
+  const bookProposer = await ctx.orm.Book.findById(bookInstanceProposer.bookId);
+  const sender = await ctx.orm.User.findById(bookInstanceProposer.userId);
+
+
   await match.accept();
   ctx.redirect('back');
 });
