13561a02149451de0c1c76de5b3c753ac007c7b4
Raimundo Herrera <rjherrera@uc.cl>
2018-10-28 19:34:20 -0300

    refactor(index): remove unnecesary preamble before redirect

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,12 @@
 const KoaRouter = require('koa-router');
 const pkg = require('../../package.json');
-const { Author } = require('../models');
 
 
 const router = new KoaRouter();
 
 router.get('/', async (ctx) => {
   if (ctx.state.currentUser) {
-    // console.log(ctx.state.currentUser);
-    const page = parseInt(ctx.query.page, 10) || 1;
-    const q = ctx.query.q || '';
-    const books = await ctx.orm.Book.findAll({
-      offset: (page - 1) * ctx.state.pageSize,
-      limit: ctx.state.pageSize,
-      include: [{ model: Author, as: 'author' }],
-      where: { title: { $iLike: `%${q}%` } },
-    });
-    ctx.redirect(ctx.router.url('books', {
-      books,
-      newBookPath: ctx.router.url('books-new'),
-      page,
-      q,
-      previousPagePath: ctx.router.url('books', { query: { page: page - 1, q } }),
-      nextPagePath: ctx.router.url('books', { query: { page: page + 1, q } }),
-    }));
+    ctx.redirect(ctx.router.url('books'));
   } else {
     await ctx.render('index', {
       appVersion: pkg.version,
