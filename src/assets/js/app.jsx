c0e3778d072d3fde2401bf6793fe11cb4ba4d214
jmcomber <jmcomber1@gmail.com>
2018-11-10 15:12:12 -0300

    feat(react-want): reactify want button

--- src/assets/js/app.jsx
+++ src/assets/js/app.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import RootWantIt from './want-app/components/rootWantIt';
 
 const reactAppContainer = document.getElementById('react-app');
 
+const wantItContainer = document.getElementById('want-it');
+
+
 if (reactAppContainer) {
   ReactDOM.render(<App />, reactAppContainer);
+}
+
+if (wantItContainer) {
+  ReactDOM.render(<RootWantIt serverData={wantItContainer.dataset} />, wantItContainer);
 }
---
+++ src/assets/js/want-app/components/WantIt.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Field from './Field';
+import { fetchInterest, wantBook, dontWantBook } from '../services/wants';
+
+export default class WantIt extends Component {
+  constructor(props) {
+    super(props);
+    const {
+      interestId,
+      bookId,
+    } = this.props;
+    this.state = {
+      want: interestId > 0,
+      interestId,
+      bookId,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadInterest();
+  }
+
+  async loadInterest() {
+    const { username, bookId } = this.props;
+    const interestId = await fetchInterest(username, bookId);
+    this.setState({ interestId });
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+    const { want } = this.state;
+    const { interestId } = this.state;
+    this.setState({ want: !want });
+
+    const path = '/interests/';
+
+    if (!want) {
+      const {
+        bookId,
+      } = this.state;
+      wantBook(path, bookId);
+      this.loadInterest();
+      return null;
+    }
+    if (interestId !== -1) {
+      return dontWantBook(path, interestId);
+    }
+    return null;
+  }
+
+  render() {
+    const {
+      want,
+      bookId,
+    } = this.state;
+
+    if (want) {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <input type="submit" className="button secondary" value="Don't want it" />
+        </form>
+      );
+    }
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <Field name="bookId" labelText="bookId" value={bookId} onChange={this.handleNameChange} />
+        <input type="submit" className="button secondary" value="Want it" />
+      </form>
+    );
+  }
+}
+
+WantIt.propTypes = {
+  username: PropTypes.string.isRequired,
+  interestId: PropTypes.number.isRequired,
+  bookId: PropTypes.string.isRequired,
+};
