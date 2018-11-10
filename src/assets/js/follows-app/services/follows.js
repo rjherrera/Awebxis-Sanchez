64c08d547d75b47721212fd5cabc950513894133
Javier López <javierlopez@uc.cl>
2018-11-10 13:31:13 -0300

    refactor(follow): group duplicated code follows service

--- src/assets/js/follows-app/services/follows.js
+++ src/assets/js/follows-app/services/follows.js
@@ -1,34 +1,36 @@
+async function fetchJson(path, method) {
+  const response = await fetch(path, { method, headers: { Accept: 'application/json' } });
+  return response.json();
+}
+
+async function requestFollow(currentUsername, username, method) {
+  const path = `/users/${currentUsername}/follow/${username}`;
+  return fetchJson(path, method);
+}
+
 export async function fetchFollowers(username) {
   const path = `/users/${username}/followers`;
-  const response = await fetch(path, { headers: { Accept: 'application/json' } });
-  const json = await response.json();
+  const json = await fetchJson(path, 'get');
   return json.followers;
 }
 
 export async function fetchFollowing(username) {
   const path = `/users/${username}/following`;
-  const response = await fetch(path, { headers: { Accept: 'application/json' } });
-  const json = await response.json();
+  const json = await fetchJson(path, 'get');
   return json.following;
 }
 
 export async function fetchFollow(currentUsername, username) {
-  const path = `/users/${currentUsername}/follow/${username}`;
-  const response = await fetch(path, { headers: { Accept: 'application/json' } });
-  const json = await response.json();
+  const json = await requestFollow(currentUsername, username, 'get');
   return json.follow;
 }
 
 export async function followUser(currentUsername, username) {
-  const path = `/users/${currentUsername}/follow/${username}`;
-  const response = await fetch(path, { method: 'post', headers: { Accept: 'application/json' } });
-  const json = await response.json();
+  const json = await requestFollow(currentUsername, username, 'post');
   return json.isFollowing;
 }
 
 export async function unfollowUser(currentUsername, username) {
-  const path = `/users/${currentUsername}/follow/${username}`;
-  const response = await fetch(path, { method: 'delete', headers: { Accept: 'application/json' } });
-  const json = await response.json();
+  const json = await requestFollow(currentUsername, username, 'delete');
   return json.isFollowing;
 }
