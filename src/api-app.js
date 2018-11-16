0eb236661668984a0ab8e6ffff2820abb2eeb6e0
Javier López <javierlopez@uc.cl>
2018-11-15 22:06:54 -0300

    feat(api): allow session cookies in api queries

--- src/api-app.js
+++ src/api-app.js
@@ -2,12 +2,20 @@ const jsonApiSerializer = require('jsonapi-serializer');
 const Koa = require('koa');
 const koaBody = require('koa-body');
 const koaLogger = require('koa-logger');
+const session = require('koa-session');
 const mailer = require('./mailers');
 const routes = require('./api-routes');
 const orm = require('./models');
 
 // App constructor
 const app = new Koa();
+
+app.keys = [
+  'these secret keys are used to sign HTTP cookies',
+  'to make sure only this app can generate a valid one',
+  'and thus preventing someone just writing a cookie',
+  'saying he is logged in when it\'s really not',
+];
 
 // expose ORM through context's prototype
 app.context.orm = orm;
@@ -28,6 +36,11 @@ app.use((ctx, next) => {
 
 // log requests
 app.use(koaLogger());
+
+// expose a session hash to store information across requests from same client
+app.use(session({
+  maxAge: 14 * 24 * 60 * 60 * 1000, // 2 weeks
+}, app));
 
 // parse request body
 app.use(koaBody({
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -10,11 +10,16 @@ const router = new KoaRouter();
 // unauthenticated endpoints
 router.use('/auth', authRoutes.routes());
 
-// JWT authentication without passthrough (error if not authenticated)
-router.use(jwt({ secret: process.env.JWT_SECRET, key: 'authData' }));
+// JWT authentication with passthrough (error if not authenticated)
+// ... also allow session cookies
+router.use(jwt({ secret: process.env.JWT_SECRET, passthrough: true, key: 'authData' }));
 router.use(async (ctx, next) => {
-  if (ctx.state.authData.userId) {
+  if (ctx.state.authData && ctx.state.authData.userId) {
     ctx.state.currentUser = await ctx.orm.User.findById(ctx.state.authData.userId);
+  } else if (ctx.session.userId) {
+    ctx.state.currentUser = await ctx.orm.User.findById(ctx.session.userId);
+  } else {
+    ctx.throw(401);
   }
   return next();
 });
