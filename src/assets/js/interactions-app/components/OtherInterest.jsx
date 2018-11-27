775f7c4096d973754b988eed1c3ba85b7df9460a
Raimundo Herrera <rjherrera@uc.cl>
2018-11-27 18:22:13 -0300

    feat(profile): modal to display people interested in own books

--- src/assets/js/interactions-app/components/OtherInterest.jsx
+++ src/assets/js/interactions-app/components/OtherInterest.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function OtherInterest(props) {
-  const { instance } = props;
+  const { instance, onClick } = props;
   const interestsCount = instance.book.interests.length;
   const people = interestsCount === 1 ? 'user' : 'users';
   return (
     <li>
       {instance.book.title}
       <span className="interest-count">
-        {` - ${interestsCount} ${people} interested`}
+        {` - ${interestsCount} ${people} interested `}
       </span>
+      <input
+        type="button"
+        className="button white empty borderless"
+        onClick={() => onClick(instance.book)}
+        value="more info."
+      />
     </li>
   );
 }
@@ -19,4 +25,5 @@ OtherInterest.propTypes = {
   instance: PropTypes.shape({
     id: PropTypes.number.isRequired,
   }).isRequired,
+  onClick: PropTypes.func.isRequired,
 };
--- src/assets/js/interactions-app/components/OthersInterests.jsx
+++ src/assets/js/interactions-app/components/OthersInterests.jsx
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import OtherInterest from './OtherInterest';
+import InterestedUsersModal from '../modals/InterestedUsersModal';
 
-export default function OthersInterests(props) {
-  const { instances } = props;
-  const rows = instances.map(instance => <OtherInterest instance={instance} key={instance.id} />);
-  return rows.length ? (
-    <ul>
-      {rows}
-    </ul>
-  ) : (
-    <p className="empty-message">You don&apos;t own any book</p>
-  );
+
+export default class OthersInterests extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { modalOpened: false, modalBook: null };
+    this.closeModal = this.closeModal.bind(this);
+    this.openModal = this.openModal.bind(this);
+  }
+
+  openModal(book) {
+    this.setState({ modalOpened: true, modalBook: book });
+  }
+
+  closeModal() {
+    this.setState({ modalOpened: false });
+  }
+
+  render() {
+    const { instances } = this.props;
+    const rows = instances.map(instance => (
+      <OtherInterest instance={instance} onClick={this.openModal} key={instance.id} />
+    ));
+    const { modalOpened, modalBook } = this.state;
+    return rows.length ? (
+      <React.Fragment>
+        <InterestedUsersModal
+          show={modalOpened}
+          handleClose={this.closeModal}
+          book={modalBook}
+        />
+        <ul>
+          {rows}
+        </ul>
+      </React.Fragment>
+    ) : (
+      <p className="empty-message">You don&apos;t own any book</p>
+    );
+  }
 }
 
 OthersInterests.propTypes = {
