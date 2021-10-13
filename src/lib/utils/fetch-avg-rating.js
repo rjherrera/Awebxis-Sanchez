5110a6716db347ceb164fb3bb1ffafcc8a898adf
Raimundo Herrera <rjherrera@uc.cl>
2021-10-12 23:48:03 -0300

    fix(goodreads): catch invalid api response

--- src/lib/utils/fetch-avg-rating.js
+++ src/lib/utils/fetch-avg-rating.js
@@ -4,8 +4,8 @@ const fetchAvgRating = async (book) => {
   const { isbn: bookISBN } = book;
   const key = process.env.GOODREADS_KEY;
   const url = `https://www.goodreads.com/book/review_counts.json?key=${key}&isbns=${bookISBN}`;
-  const response = await fetch(url).then(data => data.json().then(json => json));
-  return response.books[0].average_rating;
+  const response = await fetch(url).then(data => data.json()).catch(() => null);
+  return response ? response.books[0].average_rating : '-';
 };
 
 module.exports = {
